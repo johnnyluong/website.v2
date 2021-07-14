c4dcd3e2118fa64ef47cea80fb7e8d8af65ca053
Johnny Luong <thejohnnyluong@gmail.com>
2021-07-14 00:40:24 -0700

    removed boilerplate code, added temp header

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kDesktopBreakPoint = 1440.0;
+const kTabletBreakPoint = 768.0;
+
+const kSideMenuWidth = 300.0;
+const kNavigationRailWidth = 72.0;
+
+const kLightPrimaryColor = Color(0xffAFAFBF);
+const kDarkPrimaryColor = Color(0xFF1F1F1F);
+const kAccentColor = Color(0xFF4777AE);
+
+const defaultPadding = 24.0;
--- lib/main.dart
+++ lib/main.dart
@@ -1,201 +1,126 @@
-//{$ begin main.dart $}
 import 'package:flutter/material.dart';
+import 'package:website_v2/constants.dart';
 
-void main() => runApp(SignUpApp());
+// ignore: prefer_const_constructors
+void main() => runApp(PortfolioApp());
 
-class SignUpApp extends StatelessWidget {
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: "Open Sans",
+        scaffoldBackgroundColor: kDarkPrimaryColor,
+        textTheme: const TextTheme(
+          headline1: TextStyle(
+            color: kLightPrimaryColor,
+            fontSize: 27,
+            fontFamily: 'OpenSans',
+            fontWeight: FontWeight.bold,
+          ),
+          headline2: TextStyle(
+            color: kLightPrimaryColor,
+            fontSize: 40,
+            fontFamily: 'OpenSans',
+          ),
+          headline3: TextStyle(
+            color: kLightPrimaryColor,
+            fontSize: 20,
+            fontFamily: 'OpenSans',
+          ),
+          bodyText1: TextStyle(
+            color: kLightPrimaryColor,
+            fontFamily: 'OpenSans',
+            fontSize: 16,
+          ),
+        ),
+      ),
       routes: {
-        '/': (context) => SignUpScreen(),
-        '/welcome': (context) => WelcomeScreen(),
+        '/': (context) => const MainScreen(),
+        //'/photography': (context) => PhotographyScreen(),
       },
     );
   }
 }
 
-class SignUpScreen extends StatelessWidget {
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      body: Center(
-        child: SizedBox(
-          width: 400,
-          child: Card(
-            child: SignUpForm(),
-          ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding,
+                    horizontal: defaultPadding,
+                  ),
+                  child: Text(
+                    "LOGO HERE",
+                    style: Theme.of(context).textTheme.headline1,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding,
+                    horizontal: defaultPadding,
+                  ),
+                  child: Text(
+                    "toggle here",
+                    style: Theme.of(context).textTheme.bodyText1,
+                  ),
+                ),
+                const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding,
+                    horizontal: defaultPadding,
+                  ),
+                  child: Text(
+                    "about",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Text(
+                    "experience",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: defaultPadding,
+                    horizontal: defaultPadding,
+                  ),
+                  child: Text(
+                    "projects",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Text(
+                    "contact",
+                    style: Theme.of(context).textTheme.headline3,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Welcome!', style: Theme.of(context).textTheme.headline2),
-      ),
-    );
-  }
-}
-
-class SignUpForm extends StatefulWidget {
-  @override
-  _SignUpFormState createState() => _SignUpFormState();
-}
-
-class _SignUpFormState extends State<SignUpForm> {
-  final _firstNameTextController = TextEditingController();
-  final _lastNameTextController = TextEditingController();
-  final _usernameTextController = TextEditingController();
-
-  double _formProgress = 0;
-
-  void _showWelcomeScreen() {
-    Navigator.of(context).pushNamed('/welcome');
-  }
-
-  void _updateFormProgress() {
-    var progress = 0.0;
-    final controllers = [
-      _firstNameTextController,
-      _lastNameTextController,
-      _usernameTextController
-    ];
-
-    for (final controller in controllers) {
-      if (controller.value.text.isNotEmpty) {
-        progress += 1 / controllers.length;
-      }
-    }
-
-    setState(() {
-      _formProgress = progress;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      onChanged: _updateFormProgress,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          AnimatedProgressIndicator(value: _formProgress),
-          Text('Sign up', style: Theme.of(context).textTheme.headline4),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _firstNameTextController,
-              decoration: InputDecoration(hintText: 'First name'),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _lastNameTextController,
-              decoration: InputDecoration(hintText: 'Last name'),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: TextFormField(
-              controller: _usernameTextController,
-              decoration: InputDecoration(hintText: 'Username'),
-            ),
-          ),
-          TextButton(
-            style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.resolveWith(
-                  (Set<MaterialState> states) {
-                return states.contains(MaterialState.disabled)
-                    ? null
-                    : Colors.white;
-              }),
-              backgroundColor: MaterialStateProperty.resolveWith(
-                  (Set<MaterialState> states) {
-                return states.contains(MaterialState.disabled)
-                    ? null
-                    : Colors.blue;
-              }),
-            ),
-            onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
-            child: Text('Sign up'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class AnimatedProgressIndicator extends StatefulWidget {
-  final double value;
-
-  AnimatedProgressIndicator({
-    required this.value,
-  });
-
-  @override
-  State<StatefulWidget> createState() {
-    return _AnimatedProgressIndicatorState();
-  }
-}
-
-class _AnimatedProgressIndicatorState extends State<AnimatedProgressIndicator>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<Color?> _colorAnimation;
-  late Animation<double> _curveAnimation;
-
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-        duration: Duration(milliseconds: 1200), vsync: this);
-
-    final colorTween = TweenSequence([
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.red, end: Colors.orange),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.orange, end: Colors.yellow),
-        weight: 1,
-      ),
-      TweenSequenceItem(
-        tween: ColorTween(begin: Colors.yellow, end: Colors.green),
-        weight: 1,
-      ),
-    ]);
-
-    _colorAnimation = _controller.drive(colorTween);
-    _curveAnimation = _controller.drive(CurveTween(curve: Curves.easeIn));
-  }
-
-  @override
-  void didUpdateWidget(oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    _controller.animateTo(widget.value);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _controller,
-      builder: (context, child) => LinearProgressIndicator(
-        value: _curveAnimation.value,
-        valueColor: _colorAnimation,
-        backgroundColor: _colorAnimation.value?.withOpacity(0.4),
-      ),
-    );
-  }
-}
-
-// {$ end main.dart $}
-// {$ begin test.dart $}
-// // Avoid warning on "double _formProgress = 0;"
-// // ignore_for_file: prefer_final_fields
-// {$ end test.dart $}
+// how to navigate pages - create a method and call it
+//   void _showWelcomeScreen() {
+//     Navigator.of(context).pushNamed('/photography');
+//   }
